4ad8dba7727a7df5de6e297ddc8030596f09e4a2
Varun Nayak <varun5@cs.utexas.edu>
2021-06-23 23:09:13 -0500

    rmse 1/2

--- Netflix.cpp
+++ Netflix.cpp
@@ -1,1 +1,13 @@
+#include "Netflix.hpp"
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
 
+double PredictionCalculator::rmse(){
+
+}
--- Netflix.hpp
+++ Netflix.hpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class PredictionCalculator{
+    private:
+        // vector containing scores
+        vector<double> scores;
+        // vector containing predictions
+        vector<double> preds;
+    public:
 
+        double rmse() const;
+}
