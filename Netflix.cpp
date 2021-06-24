62e7c372dc6b47575a70b82c44204d7a2cbbdf95
avaneesh misra <avaneeshmisra01@gmail.com>
2021-06-24 08:12:55 -0500

    Created function to calculate RMSE

--- Netflix.cpp
+++ Netflix.cpp
@@ -8,6 +8,21 @@
 #include <vector>
 #include <unordered_map>
 
-double PredictionCalculator::rmse(){
+using namespace std;
 
+double PredictionCalculator::rmse() const
+{
+    assert(!scores.empty());
+    assert(!preds.empty());
+    double sum = 0;
+    int N = scores.size();
+    while (N > 0) {
+        sum += (scores[N] - preds[N]) * (scores[N] - preds[N]);
+        --N;
+    }
+    sum /= N;
+    double result = std::pow(sum, 0.5);
+
+    assert(result >= 0);
+    return result;
 }
