981720a90c7e107c3071468a61c0170425680ecf
Varun Nayak <varun5@cs.utexas.edu>
2021-06-30 10:37:25 -0500

    Pipeline 5

--- Netflix.hpp
+++ Netflix.hpp
@@ -4,26 +4,31 @@
 #include <unordered_map>
 using namespace std;
 class PredictionCalculator{
+  
     
-        
-    public:
-        /*
-        //Class constructor
-        //@param none
-        //@return none
-        */
-        PredictionCalculator();
-        //calculate the RMSE
-        double rmse() ;
-        // calculate average
-        double calc_average();
-        // callculates prediction   
-        double prediction(int userid, short movieid);
-
-        /*
-        //main driver function, solves program
-        //@param none
-        //@return none
-        */
-        void solve();
+  public:
+    /*
+    //Class constructor
+    //@param none
+    //@return none
+    */
+    PredictionCalculator();
+    //calculate the RMSE
+    double rmse() ;
+    // calculate average
+    double calc_average();
+    // callculates prediction 
+    double prediction(int userid, short movieid);
+    /*
+    //main driver function, solves program
+    //@param vector p, vector s
+    //@return none
+    */
+    void rmadd(vector<double> p, vector<double> s);
+    /*
+    //main driver function, solves program
+    //@param none
+    //@return none
+    */
+    void solve();
 };
